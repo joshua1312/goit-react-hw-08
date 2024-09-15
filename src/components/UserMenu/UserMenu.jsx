d8955f8a3e0f4b255a5a5c1557083e5f185aca43
Yuriy <yurius13@ukr.net>
2024-09-16 00:10:21 +0300

    Add components

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser } from '../../redux/auth/selectors';
+import { logOut } from '../../redux/auth/operations';
+import css from './UserMenu.module.css';
+
+const UserMenu = () => {
+    const dispatch = useDispatch();
+    const user = useSelector(selectUser);
+
+    return (
+        <div className={css.wrapperUserMenu}>
+            <p className={css.userNameMenu}>Welcome, {user.name}</p>
+            <button
+                className={css.buttonUserMenu}
+                type="button"
+                onClick={() => dispatch(logOut())}
+            >
+                Logout
+            </button>
+        </div>
+    );
+};
+
+export default UserMenu;
