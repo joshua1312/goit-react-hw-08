b37478194321171912362925c7c1f1182a72a674
Yuriy <yurius13@ukr.net>
2024-09-15 22:24:22 +0300

    Add pages

---
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -0,0 +1,36 @@
+import { useDispatch, useSelector } from 'react-redux';
+import DocumentTitle from '../../components/DocumentTitle';
+import { useEffect } from 'react';
+import { fetchContacts } from '../../redux/contacts/operations';
+import ContactList from '../../components/ContactList/ContactList';
+import {
+    selectContacts,
+    selectIsLoading,
+} from '../../redux/contacts/selectors';
+import SearchBox from '../../components/SearchBox/SearchBox';
+import ContactForm from '../../components/ContactForm/ContactForm';
+
+export default function ContactsPage() {
+    const dispatch = useDispatch();
+    const contacts = useSelector(selectContacts);
+    const isLoading = useSelector(selectIsLoading);
+
+    useEffect(() => {
+        dispatch(fetchContacts());
+    }, [dispatch]);
+
+    return (
+        <>
+            <DocumentTitle>Your contacts</DocumentTitle>
+            <h2 className="titleContactsPage">Phonebook</h2>
+            <ContactForm />
+            <SearchBox />
+            <div>{isLoading && 'Request in progress...'}</div>
+            {contacts.length > 0 ? (
+                <ContactList />
+            ) : (
+                <p>There are no any contacts ...</p>
+            )}
+        </>
+    );
+}
