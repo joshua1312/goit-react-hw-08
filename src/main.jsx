487f375d05329e937badc1cac95798c21dcd07c1
Yuriy <yurius13@ukr.net>
2024-09-16 20:57:03 +0300

    main update

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,25 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { HelmetProvider } from 'react-helmet-async';
+import store, { persistor } from './redux/store';
+import { Toaster } from 'react-hot-toast';
 import App from './App.jsx';
-import store from './redux/store';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <HelmetProvider>
+            <App />
+            <Toaster />
+          </HelmetProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-          {/* <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate> */}
   </StrictMode>,
 );
